8d4f9dad6c5a6e33fb1c67c1a337622cbd596ec5
KirlosNabil <102547988+KirlosNabil@users.noreply.github.com>
2022-11-16 11:00:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "BigDecimalIntClass.h"
-#include "BigReal.h"
+#include "BankingSystem.h"
 
 using namespace std;
 
 int main()
 {
-/*
-    BigReal n1 ("11.9000000000000000000000000000000001");
-    BigReal n2 ("2333333333339.1134322222222292");
-    BigReal n3 = n1 + n2;
-    cout << n3<<endl;
-    cout<<n2.after<<endl;
-*/
-
-    BigReal k("-0000000150.500000");
-    BigReal h("-000000000000.011");
-    BigReal i=k-h;
-    cout<<i<<endl;
+    BankingApplication start;
     return 0;
 }
-
-//zwedt ? fi el regex b3d el .
-//zwedt remove zeroes
